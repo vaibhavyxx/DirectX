5ea3e260665ffab70df8f878b81bb5f49e6bb16f
vaibhavyxx <vd5443@rit.edu>
2025-09-17 16:52:13 -0400

    mesh hw

---
+++ ConstantBuffer/Mesh.h
@@ -0,0 +1,28 @@
+#include "Vertex.h"
+#include <d3d11.h>
+#include <wrl/client.h>
+
+#pragma once
+class Mesh
+{
+public:
+	//Basics of OOP
+	Mesh(Vertex vertices[], int vertexCount, unsigned int indices[], int indexCount);
+	Mesh(const Mesh&) = delete;					//Removes copy constructor
+	Mesh& operator = (const Mesh&) = delete;	//Removes copy assignment constructor
+
+private:
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+
+public:
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetIndexBuffer();
+	int GetIndexCount();
+	int GetVertexCount();
+	void Draw();
+	int indicesCount;
+	int verticesCount;
+	~Mesh();
+};
+
