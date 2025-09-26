65d09345fdea1a5c4e97559d140e7a77f24d1177
vaibhavyxx <vd5443@rit.edu>
2025-09-25 23:03:07 -0400

    hopefully fixes float trunc issues

---
+++ A5/MoveThings/Transform.cpp
@@ -0,0 +1,168 @@
+#include "Transform.h"
+#include <DirectXMath.h>
+
+Transform::Transform(): 
+	f3Position(0,0,0),
+	f3Rotation(0,0,0),
+	f3Scale(1,1,1),
+	updateMatrices(false)
+{
+	//Sets world and inverse world matrix to identity matrices
+	DirectX::XMStoreFloat4x4(&world, DirectX::XMMatrixIdentity());
+	DirectX::XMStoreFloat4x4(&f4WorldInverseTranspose, DirectX::XMMatrixIdentity());
+}
+
+Transform::~Transform()
+{
+	//Nothing so far
+}
+
+DirectX::XMMATRIX Transform::XMMatrixMultipy() {
+	//The order is S* R* T 
+	DirectX::XMMATRIX mTransform = XMMatrixTranslation();
+	DirectX::XMMATRIX mScale = XMMatrixScaling();
+	DirectX::XMMATRIX mRotate = XMMatrixRotationRollPitchYaw();
+	return mScale * mRotate * mTransform;
+}
+
+void Transform::SetPosition(float x, float y, float z)
+{
+	f3Position.x = x;
+	f3Position.y = y;
+	f3Position.z = z;
+	updateMatrices = true;
+}
+
+void Transform::SetPosition(DirectX::XMFLOAT3 position)
+{
+	this->f3Position = position;
+	updateMatrices = true;
+}
+
+void Transform::SetRotation(float pitch, float yaw, float roll)
+{
+	f3Rotation.x = pitch;
+	f3Rotation.y = yaw;
+	f3Rotation.z = roll;
+	updateMatrices = true;
+}
+
+void Transform::SetRotation(DirectX::XMFLOAT3 rotation)
+{
+	this->f3Rotation = rotation;
+	updateMatrices = true;
+}
+
+void Transform::SetScale(float x, float y, float z)
+{
+	f3Scale.x = x;
+	f3Scale.y = y;
+	f3Scale.z = z;
+	updateMatrices = true;
+}
+
+void Transform::SetScale(DirectX::XMFLOAT3 scale)
+{
+	this->f3Scale = scale;
+	updateMatrices = true;
+}
+
+DirectX::XMFLOAT3 Transform::GetPosition()
+{
+	return f3Position;
+}
+
+DirectX::XMFLOAT3 Transform::GetPitchYawRoll()
+{
+	return f3Rotation;
+}
+
+DirectX::XMFLOAT3 Transform::GetScale()
+{
+	return f3Scale;
+}
+
+DirectX::XMFLOAT4X4 Transform::GetWorldMatrix()
+{
+	if (updateMatrices) {
+
+		DirectX::XMStoreFloat4x4(&world, XMMatrixMultipy());
+	}
+	return world;
+}
+
+DirectX::XMFLOAT4X4 Transform::GetWorldInverseTransposeMatrix()
+{
+	if (updateMatrices) {
+		DirectX::XMMATRIX mWorld = DirectX::XMLoadFloat4x4(&world);
+		DirectX::XMStoreFloat4x4(&f4WorldInverseTranspose,
+			DirectX::XMMatrixInverse(0, DirectX::XMMatrixTranspose(mWorld)));
+	}
+	return f4WorldInverseTranspose;
+}
+
+void Transform::MoveAbsolute(float x, float y, float z)
+{
+	f3Position.x += x;
+	f3Position.y += y;
+	f3Position.z += z;
+}
+
+void Transform::MoveAbsolute(DirectX::XMFLOAT3 offset)
+{
+	DirectX::XMVECTOR tPos = DirectX::XMLoadFloat3(&f3Position);
+	DirectX::XMVECTOR tOffset = DirectX::XMLoadFloat3(&offset);
+	tPos = DirectX::XMVectorAdd(tPos, tOffset);
+	DirectX::XMStoreFloat3(&f3Position, tPos);
+}
+
+void Transform::Rotate(float pitch, float yaw, float roll)
+{
+	f3Rotation.x += pitch;
+	f3Rotation.y += yaw;
+	f3Rotation.z += roll;
+}
+
+void Transform::Rotate(DirectX::XMFLOAT3 rotation)
+{
+	DirectX::XMVECTOR tRot = DirectX::XMLoadFloat3(&f3Rotation);
+	DirectX::XMVECTOR tOffset = DirectX::XMLoadFloat3(&rotation);
+	tRot = DirectX::XMVectorAdd(tRot, tOffset);
+	DirectX::XMStoreFloat3(&f3Rotation, tRot);
+}
+
+void Transform::Scale(float x, float y, float z)
+{
+	f3Scale.x *= x;
+	f3Scale.y *= y;
+	f3Scale.z *= z;
+}
+
+void Transform::Scale(DirectX::XMFLOAT3 scale)
+{
+	DirectX::XMVECTOR tScale = DirectX::XMLoadFloat3(&f3Scale);
+	DirectX::XMVECTOR tOffset = DirectX::XMLoadFloat3(&scale);
+	tScale = DirectX::XMVectorMultiply(tScale, tOffset);
+	DirectX::XMStoreFloat3(&f3Scale, tScale);
+}
+
+DirectX::XMMATRIX Transform::XMMatrixTranslation()
+{
+	DirectX::XMVECTOR vTranslate = DirectX::XMLoadFloat3(&f3Position);
+	DirectX::XMMATRIX mTranslate = DirectX::XMMatrixTranslationFromVector(vTranslate);
+	return mTranslate;
+}
+
+DirectX::XMMATRIX Transform::XMMatrixScaling()
+{
+	DirectX::XMVECTOR vScale = DirectX::XMLoadFloat3(&f3Scale);
+	DirectX::XMMATRIX mScale = DirectX::XMMatrixScalingFromVector(vScale);
+	return mScale;
+}
+
+DirectX::XMMATRIX Transform::XMMatrixRotationRollPitchYaw()
+{
+	DirectX::XMVECTOR vRotate = DirectX::XMLoadFloat3(&f3Rotation);
+	DirectX::XMMATRIX mRotate = DirectX::XMMatrixTranslationFromVector(vRotate);
+	return mRotate;
+}
---
+++ A5/MoveThings/VertexShader.hlsl
@@ -0,0 +1,54 @@
+cbuffer ExternalDatat : register(b0)
+{
+    float4 colorTint;
+    float4x4 world;
+}
+
+// Struct representing a single vertex worth of data
+// - This should match the vertex definition in our C++ code
+// - By "match", I mean the size, order and number of members
+// - The name of the struct itself is unimportant, but should be descriptive
+// - Each variable must have a semantic, which defines its usage
+struct VertexShaderInput
+{ 
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float3 localPosition	: POSITION;     // XYZ position
+	float4 color			: COLOR;        // RGBA color
+};
+
+// Struct representing the data we're sending down the pipeline
+// - Should match our pixel shader's input (hence the name: Vertex to Pixel)
+// - At a minimum, we need a piece of data defined tagged as SV_POSITION
+// - The name of the struct itself is unimportant, but should be descriptive
+// - Each variable must have a semantic, which defines its usage
+struct VertexToPixel
+{
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float4 screenPosition	: SV_POSITION;	// XYZW position (System Value Position)
+	float4 color			: COLOR;        // RGBA color
+};
+
+// --------------------------------------------------------
+// The entry point (main method) for our vertex shader
+// 
+// - Input is exactly one vertex worth of data (defined by a struct)
+// - Output is a single struct of data to pass down the pipeline
+// - Named "main" because that's the default the shader compiler looks for
+// --------------------------------------------------------
+VertexToPixel main( VertexShaderInput input )
+{
+	// Set up output struct
+	VertexToPixel output;
+    //float3 movedPos = input.localPosition + offset;
+    output.screenPosition = mul(world, float4(input.localPosition, 1.0f));
+    output.color = input.color * colorTint;
+	return output;
+}
