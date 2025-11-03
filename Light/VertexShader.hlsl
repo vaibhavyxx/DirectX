a805aa0598244f4ed0f1b797e43f578d822e524d
vaibhavyxx <vd5443@rit.edu>
2025-11-02 19:46:44 -0500

    made a new copy of lights

---
+++ Light/VertexShader.hlsl
@@ -0,0 +1,61 @@
+cbuffer ExternalData : register(b0)
+{
+    matrix world;
+    matrix view;
+    matrix projection;
+}
+
+// Struct representing a single vertex worth of data
+// - This should match the vertex definition in our C++ code
+// - By "match", I mean the size, order and number of members
+// - The name of the struct itself is unimportant, but should be descriptive
+// - Each variable must have a semantic, which defines its usage
+struct VertexShaderInput
+{ 
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float3 localPosition	: POSITION;     // XYZ position
+    float2 uv				: TEXCOORD;
+    float3 normal			: NORMAL;
+};
+
+// Struct representing the data we're sending down the pipeline
+// - Should match our pixel shader's input (hence the name: Vertex to Pixel)
+// - At a minimum, we need a piece of data defined tagged as SV_POSITION
+// - The name of the struct itself is unimportant, but should be descriptive
+// - Each variable must have a semantic, which defines its usage
+struct VertexToPixel
+{
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float4 screenPosition	: SV_POSITION;	// XYZW position (System Value Position)
+    float2 uv				: TEXCOORD;
+    float3 normal			: NORMAL;
+	
+};
+
+// --------------------------------------------------------
+// The entry point (main method) for our vertex shader
+// 
+// - Input is exactly one vertex worth of data (defined by a struct)
+// - Output is a single struct of data to pass down the pipeline
+// - Named "main" because that's the default the shader compiler looks for
+// --------------------------------------------------------
+VertexToPixel main( VertexShaderInput input )
+{
+	// Set up output struct
+	VertexToPixel output;
+    //float3 movedPos = input.localPosition + offset;
+   	matrix wvp = mul(projection, mul(view, world));
+    output.screenPosition = mul(wvp, float4(input.localPosition, 1.0f));
+    
+	output.uv = input.uv;
+    output.normal = input.normal;
+	return output;
+}
